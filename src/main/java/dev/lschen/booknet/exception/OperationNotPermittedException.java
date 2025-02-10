2ca90e1615388905ad97961918632619805b26f0
Lisa Chen <lisachen011299@gmail.com>
2025-02-10 12:21:32 +0000

    refactor to divde into frontend and backend

--- src/main/java/dev/lschen/booknet/exception/OperationNotPermittedException.java
+++
@@ -1,7 +0,0 @@
-package dev.lschen.booknet.exception;
-
-public class OperationNotPermittedException extends RuntimeException {
-    public OperationNotPermittedException(String message) {
-        super(message);
-    }
-}
